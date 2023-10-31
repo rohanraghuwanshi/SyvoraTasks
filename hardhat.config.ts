759a035d0b94c4ed3b492a6c197771b60f2a2755
Rohan Raghuwanshi <rohanraghuvanshi001@gmail.com>
2023-10-31 11:50:36 +0530

    Feat: using env variables now

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
+require('dotenv').config();
 import "@nomicfoundation/hardhat-toolbox";
 
-const ALCHEMY_API_KEY = "Qh72n1XLdTvH8ExtsozHk6W1OwxuEJiL"
-const GOERLI_PRIVATE_KEY = ""
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || ''
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
-    goelri: {
+    goerli: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY]
     }
