3cdd7e7c6aa9702fcb8f1e2250b9dc9cbae55ec5
Rohan Raghuwanshi <rohanraghuvanshi001@gmail.com>
2023-10-31 12:21:36 +0530

    Add deploy script to deploy all 3 contracts in one go

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
-require('dotenv').config();
 import "@nomicfoundation/hardhat-toolbox";
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+require('dotenv').config();
+
+const ALCHEMY_API_URL_GOERLI = process.env.ALCHEMY_API_URL_GOERLI
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || ''
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     goerli: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      url: ALCHEMY_API_URL_GOERLI,
       accounts: [GOERLI_PRIVATE_KEY]
     }
   },
